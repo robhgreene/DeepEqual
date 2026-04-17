332884bd8ef10015c9d66caf79f2adaac2c984ce
Robert Greene <robhgreene@gmail.com>
2026-04-16 17:53:25 -0700

    First Commit

---
+++ deepEqual.js
@@ -0,0 +1,12 @@
+'use strict';
+// Don't add or change anything above this comment.
+
+/*
+* Don't change the declaration of this function.
+*/
+function deepEqual(val1, val2) {
+
+}
+
+// Don't add or change anything below this comment.
+module.exports = deepEqual;
